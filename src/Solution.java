73ecfe162b1fcde7319a46a1732fcb3a9efbbe9d
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-28 22:53:16 +0530

    added

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,30 @@
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // TODO: Read the integer N
-        
-        // TODO: Create a HashSet to store the integers
-        
-        // TODO: Loop N times to read the integers and add them to the HashSet
-        
-        // TODO: Iterate through the HashSet and print the unique numbers (space-separated)
-        
-        scanner.close();
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        int count = 0;
+        int size = set.size();
+
+        for (Integer num : set) {
+            System.out.print(num);
+            count++;
+            if (count < size) {
+                System.out.print(" ");
+            }
+        }
+
+        sc.close();
     }
 }
